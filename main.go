c61529deb36fb440bc9b7bb179427dae44f3d948
Aron Nosay <noszalyaron4@gmail.com>
2015-01-01 11:15:34 +0100

    add option -versbose to test and small rework

--- main.go
+++ main.go
@@ -92,12 +92,12 @@ func HandleBoolError(b bool, message string) {
 }
 
 type Printer interface {
-	Print(*io.Writer, []Test)
+	Print(io.Writer, []Test)
 }
 
 type PrettyPrinter struct{}
 
-func (p PrettyPrinter) Print(w *io.Writer, tests []Test) {
+func (p PrettyPrinter) Print(w io.Writer, tests []Test) {
 	for _, t := range tests {
 		fmt.Fprintf(w, "%s\nOutput\n======\n%s\n\n\n", t.Input, t.Output)
 	}
@@ -105,10 +105,87 @@ func (p PrettyPrinter) Print(w *io.Writer, tests []Test) {
 
 type JsonPrinter struct{}
 
-func (j JsonPrinter) Print(w *io.Writer, tests []Test) {
+func (j JsonPrinter) Print(w io.Writer, tests []Test) {
 	out, err := json.Marshal(tests)
-	HandleError(err, "nem tudtam létrehozni a json objektumot:", err.Error())
-	fmt.Fprintln(string(out))
+	HandleError(err, "nem tudtam létrehozni a json objektumot:"+err.Error())
+	fmt.Fprintln(w, string(out))
+}
+
+func Pio(c *cli.Context) {
+	ValidateArgs(len(c.Args()), 2)
+
+	problem, _ := strconv.Atoi(c.Args()[0])
+	subproblem := c.Args()[1]
+
+	HandleBoolError((problem < 1), "Hiba a probléma sorszáma nem lehet 1-nél kisebb!")
+
+	tests, err := GetProblem(problem, subproblem)
+	HandleError(err, err.Error())
+
+	var printer Printer
+
+	if c.Bool("json") {
+		printer = JsonPrinter{}
+	} else {
+		printer = PrettyPrinter{}
+	}
+
+	printer.Print(os.Stdout, tests)
+}
+
+const (
+	TimeLimitExceed = iota
+	WrongAnswer
+	Accepted
+	End
+)
+
+type TesterOptions struct {
+	w         io.Writer
+	binary    string
+	timelimit int
+	verbose   bool
+	t         Test
+}
+
+func Tester(to TesterOptions) int {
+	cmd := exec.Command("./" + to.binary)
+
+	var out bytes.Buffer
+
+	cmd.Stdin = strings.NewReader(to.t.Input)
+	cmd.Stdout = &out
+
+	err := cmd.Start()
+	HandleError(err, "CANNOT START EXECUTABLE")
+
+	l := make(chan bool, 1)
+	go func(cmd *exec.Cmd, t chan bool) {
+		cmd.Wait()
+		t <- true
+	}(cmd, l)
+
+	select {
+	case <-l:
+		o, _ := ioutil.ReadAll(&out)
+		if !to.verbose {
+			fmt.Fprintf(to.w, "\nANSWER\n")
+			fmt.Fprintf(to.w, "======\n\n%s", to.t.Output)
+			fmt.Fprintf(to.w, "\n\nYOUR ANSWER\n")
+			fmt.Fprintf(to.w, "===========\n\n%s\n\n", o)
+		}
+		if RemoveWhitespaces(string(o)) == RemoveWhitespaces(to.t.Output) {
+			return Accepted
+		} else {
+			return WrongAnswer
+
+		}
+	case <-time.After(time.Duration(int(time.Millisecond) * to.timelimit)):
+		return TimeLimitExceed
+		cmd.Process.Kill()
+	}
+
+	return End
 }
 
 func main() {
@@ -127,58 +204,27 @@ func main() {
 					Usage: "turn it on to have json output",
 				},
 			},
-			Action: func(c *cli.Context) {
-				ValidateArgs(len(c.Args()), 2)
-
-				problem, _ := strconv.Atoi(c.Args()[0])
-				subproblem := c.Args()[1]
-
-				HandleErrorBool((problem < 1), "Hiba a probléma sorszáma nem lehet 1-nél kisebb!")
-
-				tests, err := GetProblem(problem, subproblem)
-				HandleError(err, err.Error())
-
-				var printer Printer
-
-				if c.Bool("json") {
-					printer = JsonPrinter{}
-				} else {
-					printer = PrettyPrinter{}
-				}
-
-				printer.Print(os.Stdout, tests)
-			},
+			Action: Pio,
 		},
 		{
 			Name:  "init",
 			Usage: "egy fájlt inicializál a " + Default + " alapján",
 			Action: func(c *cli.Context) {
 				ValidateArgs(len(c.Args()), 1)
-				//if len(c.Args()) < 1 {
-				//	fmt.Println("Hiba meg kell adni egy fájl nevet ahova inicializálni akarjuk az alap.cpp-t")
-				//	return
-				//}
 
 				file, err := os.Create(c.Args()[0])
-				if err != nil {
-					fmt.Println("Hiba nem tudom megnyitni a fájlt")
-					return
-				}
+				HandleError(err, "Hiba nem tudom megnyitni a fájlt")
 				defer file.Close()
 
 				d, err := os.Open(Default)
-				if err != nil {
-					fmt.Println("Hiba nem tudom megnyitni az alapértelmezett fájlt")
-					return
-				}
+				HandleError(err, "Hiba nem tudom megnyitni az alapértelmezett fájlt")
 				defer d.Close()
 
 				dcontent, err := ioutil.ReadAll(d)
-				if err != nil {
-					fmt.Println("Hiba nem tudom beolvasni az alapértelmezett fájlt")
-					return
-				}
-				file.Write(dcontent)
+				HandleError(err, "Hiba nem tudom beolvasni az alapértelmezett fájlt")
+
+				_, err = file.Write(dcontent)
+				HandleError(err, "Hiba nem tudom az alapértelmezett fájl tartalmát beleírni a fájlba")
 			},
 		},
 		{
@@ -190,62 +236,39 @@ func main() {
 					Value: 1000,
 					Usage: "time limit in milliseconds",
 				},
+				cli.BoolFlag{
+					Name:  "verbose",
+					Usage: "ne legyen sok output",
+				},
 			},
 			Action: func(c *cli.Context) {
 				ValidateArgs(len(c.Args()), 2)
 
 				file, err := os.Open(c.Args()[0])
-				if err != nil {
-					fmt.Println("Hiba nem tudom megnyitni a json-t!")
-					return
-				}
+				HandleError(err, "Hiba nem tudom megnyitni a json-t!")
+
 				content, err := ioutil.ReadAll(file)
-				if err != nil {
-					fmt.Println("Hiba nem tudom beolvasni a json-t!")
-				}
+				HandleError(err, "Hiba nem tudom beolvasni a json-t!")
 
 				var tests []Test
 				err = json.Unmarshal(content, &tests)
-				if err != nil {
-					fmt.Println("Nem sikerült dekódolni a json fájlt!")
-				}
+				HandleError(err, "Nem sikerült dekódolni a json fájlt!")
 
+				to := TesterOptions{w: os.Stdout, binary: c.Args()[1], timelimit: c.Int("timelimit"), verbose: c.Bool("verbose")}
 				for i, t := range tests {
+					to.t = t
 					fmt.Printf("TEST #%d\n", i+1)
-					cmd := exec.Command("./" + c.Args()[1])
 
-					cmd.Stdin = strings.NewReader(t.Input)
-					var out bytes.Buffer
-					cmd.Stdout = &out
-
-					err := cmd.Start()
-					if err != nil {
-						fmt.Println("CANNOT START EXECUTABLE")
-						continue
-					}
-					l := make(chan bool, 1)
-
-					go func(cmd *exec.Cmd, t chan bool) {
-						cmd.Wait()
-
-						t <- true
-					}(cmd, l)
-
-					select {
-					case <-l:
-
-						o, _ := ioutil.ReadAll(&out)
-						if RemoveWhitespaces(string(o)) == RemoveWhitespaces(t.Output) {
-							fmt.Println("[AC]")
-						} else {
-							fmt.Println("[WA]")
-						}
-					case <-time.After(time.Duration(int(time.Millisecond) * c.Int("timelimit"))):
+					output := Tester(to)
+					switch output {
+					case WrongAnswer:
+						fmt.Println("[WA]")
+					case TimeLimitExceed:
 						fmt.Println("[TE]")
-						o, _ := ioutil.ReadAll(&out)
-						fmt.Println(string(o))
-						continue
-
+					case Accepted:
+						fmt.Println("[AC]")
+					case End:
+						fmt.Println("[??]")
 					}
 				}
 
